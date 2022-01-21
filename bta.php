35bc74a30773f19dd70147645b59138ebd675b1f
Julian Tovar <julian.tovar@openlms.net>
2022-01-21 16:38:10 -0500

    OKR-6: bta in progress

---
+++ bta.php
@@ -0,0 +1,12 @@
+<?php
+// Perform the XML file analysis
+
+function parse_behat_xml($branch = "master", $from = null, $to = null) {
+    //$file = "tmp/" . $branch . $add_var_here_please;
+    unzip_file();
+}
+
+function unzip_file() {
+    // implement unzipping (the resulting dir must have the same name of the zip file,
+    // not the name 'junit_files' that it has by default
+}
